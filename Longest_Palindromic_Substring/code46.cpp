80821baa5a1cabcd39fcd1f3cf562a394fc0f5e3
Atharva Kokate <61040145+atharvakokatee@users.noreply.github.com>
2020-04-13 14:54:01 +0530

    done

---
+++ Longest_Palindromic_Substring/code46.cpp
@@ -0,0 +1,37 @@
+// Longest Palindromic Substring
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string longestString(string &s){
+    string best_s="";
+    for(int mid=0;mid<s.size();mid++){
+        for(int x=0;mid-x>=0 && mid+x<s.size();x++){
+            if(s[mid-x] != s[mid+x])
+                break;
+            int len = 2 * x + 1;
+            if(len > best_s.size()){
+                best_s = s.substr(mid-x,len);
+            }    
+        }
+    }
+    for(int mid=0;mid<s.size();mid++){
+        for(int x=0;mid-x+1>=0 && mid+x<s.size();x++){
+            cout<<" mid-x+1 "<<s[mid-x+1]<<" mid+x "<<s[mid+x]<<endl;
+            if(s[mid-x+1]!=s[mid+x])
+                break;
+            int len = 2 * x;
+            if(len > best_s.size()){
+                best_s = s.substr(mid-x+1,len);
+            }    
+        }
+    }
+    return best_s;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    string result = longestString(s);
+    cout<<result;
+}
